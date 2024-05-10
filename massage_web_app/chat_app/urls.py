db60e8f92a5813027a369ebc481e5bcd4a13091d
Al Arman Ovi <ovibaridar06@gmail.com>
2024-05-11 02:12:56 +0600

    update

--- massage_web_app/chat_app/urls.py
+++ massage_web_app/chat_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
+from . import chat
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('sign up', views.signup, name='sign up'),
-    path('creat_acount', views.creat_acount, name='creat_acount')
+    path('creat_acount', views.creat_acount, name='creat_acount'),
+    path('chat', chat.chat, name='chat'),
+    path('login', views.log_in_, name='login')
 
 ]
--- massage_web_app/chat_app/views.py
+++ massage_web_app/chat_app/views.py
@@ -1,10 +1,36 @@
+from django.contrib.auth import login
+from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 
-# Create your views here.
-
 def home(request):
+    if request.user.is_authenticated:
+        return redirect("chat")
+    else:
+        return render(request, 'chat_app/signin.html')
+
+
+def log_in_(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Retrieve the user based on the email address
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user = None
+
+        if user is not None and check_password(password, user.password):
+            # Password is correct, log in the user
+            login(request, user)
+            return redirect('chat')
+        else:
+            # Incorrect email or password
+            error = 'Invalid email or password. Please try again.'
+            return render(request, 'chat_app/signin.html', {'error': error})
+
     return render(request, 'chat_app/signin.html')
 
 
@@ -19,14 +45,17 @@ def creat_acount(request):
         password = request.POST.get('password')
         con_p = request.POST.get('confirm_password')
 
-        # You may want to add validation and error handling here
+        if User.objects.filter(username=name).exists():
+            error = "This username is already in use."
+            return render(request, 'chat_app/register.html', {'error': error})
+
         if User.objects.filter(email=gmail).exists():
             error = "This email is already in use."
             return render(request, 'chat_app/register.html', {'error': error})
-        # Save the data to the database using the renamed model
-        else:
-            my_user = User.objects.create_user(name, gmail, password)
-            my_user.save()
-            return redirect('home')
 
-    return render(request, 'vote_application/Sign_up_.html')
+        # Save the data to the database using the renamed model
+        my_user = User.objects.create_user(name, gmail, password)
+        my_user.save()
+        return redirect('home')
+
+    return render(request, 'chat_app/register.html')
